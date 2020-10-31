d70ab79b1cce69e56f4ba311d208cd366d7c102a
Vito De Tullio <vito.detullio@gmail.com>
2020-10-31 11:15:40 +0100

    working on refactoring - move static content away

--- moves/web.py
+++ moves/web.py
@@ -1,32 +1,33 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, current_app
 
 from . import configurations
 
 
 class Web(Flask):
     def __init__(self):
-        super().__init__(__name__)
+        super().__init__(__name__, static_url_path='/')
 
         @self.route('/')
         def index():
-            return render_template('index.html')
-
-        @self.route('/favicon.ico')
-        def favicon():
-            return ''
-
-        @self.route('/<path>')
-        def web(path):
-            return render_template(path,
-                                   rest_hostname=configurations.REST_HOSTNAME,
-                                   rest_port=configurations.REST_PORT,
-                                   stomp_port=configurations.STOMP_PORT,
-                                   ws_port=configurations.WS_PORT,
-                                   amq_hostname=configurations.AMQ_HOSTNAME,
-                                   amq_username=configurations.AMQ_USERNAME,
-                                   amq_passcode=configurations.AMQ_PASSCODE,
-                                   amq_queue=configurations.AMQ_QUEUE)
+            return current_app.send_static_file('index.html')
+#             return render_template('index.html')
+# 
+#         @self.route('/favicon.ico')
+#         def favicon():
+#             return ''
+# 
+#         @self.route('/<path>')
+#         def web(path):
+#             return render_template(path,
+#                                    rest_hostname=configurations.REST_HOSTNAME,
+#                                    rest_port=configurations.REST_PORT,
+#                                    stomp_port=configurations.STOMP_PORT,
+#                                    ws_port=configurations.WS_PORT,
+#                                    amq_hostname=configurations.AMQ_HOSTNAME,
+#                                    amq_username=configurations.AMQ_USERNAME,
+#                                    amq_passcode=configurations.AMQ_PASSCODE,
+#                                    amq_queue=configurations.AMQ_QUEUE)
 
     def run(self):
         return super().run(port=configurations.WEB_PORT)
