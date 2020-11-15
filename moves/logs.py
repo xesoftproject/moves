544dcc56ccd9c049627d4e56d888c205c62e7a25
Vito De Tullio <vito.detullio@gmail.com>
2020-11-15 12:35:02 +0100

    seems it goes

--- moves/logs.py
+++ moves/logs.py
@@ -1,5 +1,6 @@
 import logging.config
 import logging.handlers
+import os
 
 from . import configurations
 
@@ -7,16 +8,23 @@ from . import configurations
 def _fqcn(cls: type) -> str:
     return f'{cls.__module__}.{cls.__qualname__}'
 
+
+LOGS_ROOT = 'logs'
+
+
 def setup_logs():
-    if True or configurations.running_on_ec2():
+    if configurations.running_on_ec2():
         # write logs on file
+
+        # ensure the directory exists
+        os.makedirs(LOGS_ROOT, exist_ok=True)
 
         logging.config.dictConfig({
             'version': 1,
             'handlers': {
                 'wsgi': {
                     'class': _fqcn(logging.handlers.TimedRotatingFileHandler),
-                    'filename': f'logs/{__name__}.log',
+                    'filename': f'{LOGS_ROOT}/{__name__}',
                     'when': 'midnight',
                     'backupCount': 1
                 }
