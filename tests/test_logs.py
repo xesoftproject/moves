78612dcc40bc17bdeb1b0a773541fa04a3fe6312
Vito De Tullio <vito.detullio@gmail.com>
2021-02-03 08:42:50 +0100

    still hangs

--- tests/test_logs.py
+++ tests/test_logs.py
@@ -1,18 +1,12 @@
 from logging import INFO
 from logging import getLogger
-from os import rmdir, remove
-from os.path import join
 from unittest import TestCase
 
 from quart_trio.app import QuartTrio
 
-from moves.logs import LOGS_ROOT
-from moves.logs import setup_logs
-
 from ._support_for_tests import trio_test
 
 
-setup_logs('delme.log', True)
 LOGS = getLogger(__name__)
 
 
@@ -33,7 +27,3 @@ class TestLogs(TestCase):
         client = app.test_client()
         with self.assertLogs(LOGS, INFO):
             await client.get('/foo')
-
-    def tearDown(self) -> None:
-        remove(join(LOGS_ROOT, 'delme.log'))
-        rmdir(LOGS_ROOT)
